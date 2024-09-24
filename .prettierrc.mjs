b3a2dab8854178bed16782752f7bb6499657077f
Nick Gernert <nickgernert@gmail.com>
2024-09-23 21:01:53 -0400

    Prettier format

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -3,10 +3,11 @@
  * @type {import("prettier").Config}
  */
 const config = {
-  trailingComma: "es5",
-  tabWidth: 4,
-  semi: false,
-  singleQuote: true,
-};
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    plugins: ['prettier-plugin-tailwindcss'],
+}
 
-export default config;
+export default config
