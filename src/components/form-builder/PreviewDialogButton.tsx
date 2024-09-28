1f3dc294067492f91da9da6e4da28fddf11be3d6
Aaron Smith <binaryfabricator@gmail.com>
2024-09-28 11:33:21 -0400

    Formatter udpates.

--- src/components/form-builder/PreviewDialogButton.tsx
+++ src/components/form-builder/PreviewDialogButton.tsx
@@ -2,8 +2,11 @@ import { Button } from '@/components/ui/button'
 import React from 'react'
 
 import { Eye } from 'lucide-react'
+import { useDesigner } from './designer/DesignerContext'
 
 function PreviewDialogButton() {
+  const { controls } = useDesigner()
+
   return (
     <Button variant={'outline'} className="gap-2">
       <Eye className="h-4 w-4" />
